31b51f2663ecf1cf2d85dffc821beda7a671ac15
Maciek <maciejlizuraj@gmail.com>
2024-01-02 18:57:24 +0100

    Added data reading

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Data_science_club_classical_ML
+
+Data source: 
+https://archive.ics.uci.edu/dataset/16/breast+cancer+wisconsin+prognostic
---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def read_data():
+    data_file = 'data/wpbc.data'
+    column_names = ['ID number', 'Outcome', 'Time', 'Nucleus 1 radius', 'Nucleus 1 texture', 'Nucleus 1 perimeter',
+                    'Nucleus 1 area', 'Nucleus 1 smoothness', 'Nucleus 1 compactness', 'Nucleus 1 concavity',
+                    'Nucleus 1 concave points', 'Nucleus 1 symmetry', 'Nucleus 1 fractal dimension', 'Nucleus 2 radius',
+                    'Nucleus 2 texture', 'Nucleus 2 perimeter', 'Nucleus 2 area', 'Nucleus 2 smoothness',
+                    'Nucleus 2 compactness', 'Nucleus 2 concavity', 'Nucleus 2 concave points', 'Nucleus 2 symmetry',
+                    'Nucleus 2 fractal dimension', 'Nucleus 3 radius', 'Nucleus 3 texture', 'Nucleus 3 perimeter',
+                    'Nucleus 3 area', 'Nucleus 3 smoothness', 'Nucleus 3 compactness', 'Nucleus 3 concavity',
+                    'Nucleus 3 concave points', 'Nucleus 3 symmetry', 'Nucleus 3 fractal dimension', 'Tumor size',
+                    'Lymph node status']
+
+    data_frame = pd.read_csv(data_file, names=column_names)
+    return data_frame
+
+
+if __name__ == '__main__':
+    print(read_data())

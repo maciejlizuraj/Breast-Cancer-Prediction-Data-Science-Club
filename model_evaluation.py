872a6c59e1d1ea45c896d6fd80e339e4f2796edd
lenamiriam <108835852+lenamiriam@users.noreply.github.com>
2024-01-13 16:00:46 +0100

    Updated model_evaluation.py

--- model_evaluation.py
+++ model_evaluation.py
@@ -6,16 +6,16 @@ class ModelEvaluator:
         self.X_test = X_test
         self.y_test = y_test
 
-    def display_evaluation(self, model):
+    def evaluate_model(self, model):
         y_pred = model.predict(self.X_test)
         accuracy = round(accuracy_score(self.y_test, y_pred), 4)
         precision = round(precision_score(self.y_test, y_pred), 4)
         recall = round(recall_score(self.y_test, y_pred), 4)
         f1 = round(f1_score(self.y_test, y_pred), 4)
 
-        print('Evaluation Metrics:')
-        print(f'Accuracy: {accuracy}')
-        print(f'Precision: {precision}')
-        print(f'Recall: {recall}')
-        print(f'F1 Score: {f1}')
-        print()
+        return {
+            'Accuracy': accuracy,
+            'Precision': precision,
+            'Recall': recall,
+            'F1 Score': f1
+        }
